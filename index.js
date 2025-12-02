f210beef5f76c2a5cf11b9d39ac459b2e1c5b57f
C10H12O2 <Eugene.emmanuel2007@gmail.com>
2025-12-02 20:33:00 +0300

    Added an if statement block that created a block-scoped variable and assigns it the value 'Maple Bacon Burger' and uses the push array method to add named variable to the burgers array

--- index.js
+++ index.js
@@ -6,3 +6,8 @@ function addBurger(){
     let newBurger = 'FlatBurger';
     burgers.push(newBurger);
 }
+
+if(true){
+    let anotherNewBurger = 'Maple Bacon Burger';
+    burgers.push(anotherNewBurger);
+}
